b66fc656f2490e70b114a50c9a896a147ea22b3c
ems <Password@123>
2021-04-28 22:49:41 +0530

    test cont added

--- src/components/TestContainer/TestContainer.jsx
+++ src/components/TestContainer/TestContainer.jsx
@@ -4,9 +4,12 @@ import Tryagain from "../TryAgain/TryAgain"
 const TestContainer = ({words, characters, wpm}) =>{
     return(
         <div className="test-container">
-            <div className="try-again-con">
-                < Tryagain words={words} characters = {characters} wpm = {wpm}/>
+            <div data-aos="fade-up" className="typing-challenge-cont">
+                <h1>This is Test</h1>
             </div>
+            {/* <div className="try-again-con">
+                < Tryagain words={words} characters = {characters} wpm = {wpm}/>
+            </div> */}
         </div>
     )
 }
